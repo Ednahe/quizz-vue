4243c47de66e475543b3e8b58f42af6d9d244de5
Nashsan <emmanuel.donahue@hotmail.fr>
2025-01-08 16:00:47 +0100

    suite

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -7,11 +7,25 @@ const props = defineProps({
 <template>
     <div class="question">
         <h3>{{ question.question }}</h3>
+        <ul>
+            <li v-for="(choices, index) in question.choices" :key="choices">
+                <label :for="`reponse${index}`">
+                    <input :id="`reponse${index}`" type="radio" name="reponse">
+                    {{ choices }}
+                </label>
+            </li>
+        </ul>
+        <button>Question suivante</button>
     </div>
 </template>
 
 <style>
 .question {
     padding-top: 2rem;
+}
+
+.question button {
+    margin-left: auto;
+    display: block;
 }
 </style>
