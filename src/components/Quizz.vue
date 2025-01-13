69a4da252a8e0f64d7aff7d80a08c7d7a3e8f2d7
Nashsan <emmanuel.donahue@hotmail.fr>
2025-01-13 16:49:44 +0100

    suite

--- src/components/Quizz.vue
+++ src/components/Quizz.vue
@@ -2,19 +2,33 @@
 import { computed, ref } from "vue";
 import Progress from "./Progress.vue"
 import Question from "./Question.vue";
+import Recap from "./Recap.vue";
 
 const props = defineProps({
     quizz: Object
 })
 
+const reponses = ref(props.quizz.questions.map(() => null));
+const state = ref('question');
 const step = ref(0);
-const question = computed(() => props.quizz.questions[step.value])
+const question = computed(() => props.quizz.questions[step.value]);
+
+const addReponse = (reponse) => {
+    reponses.value[step.value] = reponse;
+    if (step.value === props.quizz.questions.length - 1) {
+        state.value = 'recap';
+    } else {
+        step.value++;
+    }
+}
 </script>
 
 <template>
     <div>
         <h1>{{ quizz.title }}</h1>
         <Progress :value="step" :max="quizz.questions.length" />
-        <Question :question="question" v-if="question" />
+        <Question :key="question.question" :question="question" v-if="state === 'question'" @reponse="addReponse"/>
+        <Recap v-if="state === 'recap'" :reponses="reponses" :quizz="quizz"/>
+        {{ reponses }}
     </div>
 </template>
---
+++ src/components/Recap.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    quizz: Object,
+    reponses: Array
+})
+
+const score = computed(() => {
+    return props.quizz.questions.reduce((count, question, index) => {
+        if (question.correct === props.reponses[index]) {
+            return count + 1;
+        }
+        return count;
+    }, 0)
+})
+
+</script>
+
+<template>
+    <h1>Recap</h1>
+    <p>
+        Score : {{ score }}/{{ quizz.questions.length }}
+    </p>
+</template>
