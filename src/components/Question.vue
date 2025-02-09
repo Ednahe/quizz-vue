d3e565fbe0d98a06f4b3dd417bb62f496f569df1
Nashsan <emmanuel.donahue@hotmail.fr>
2025-02-09 15:31:20 +0100

    suite

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,38 +1,66 @@
 <script setup>
-import { ref, computed } from 'vue';
-import { shuffleArray } from '../functions/array';
-import Reponse from './Reponse.vue';
+import { ref, computed, onMounted } from "vue";
+import { shuffleArray } from "../functions/array";
+import Reponse from "./Reponse.vue";
 
 const props = defineProps({
-    question: Object
+  question: Object,
+});
+
+const emits = defineEmits(["reponse"]);
+const reponse = ref(null);
+const noReponse = computed(() => reponse.value !== null);
+const randomChoices = computed(() => shuffleArray(props.question.choices));
+
+let timer;
+
+const onReponse = (e) => {
+    reponse.value = e.currentTarget.value;
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+        emits('reponse', reponse.value)
+    }, 1000)
+}
+
+onMounted(() => {
+    timer = setTimeout(() => {
+        emits('reponse', reponse.value)
+    }, 3000)
 })
 
-const emits = defineEmits(['reponse']);
-const reponse = ref(null);
-const noReponse = computed(() => reponse.value !== null)
-const randomChoices = computed(() => shuffleArray(props.question.choices))
+onMounted(() => {
+    clearTimeout(timer)
+})
 </script>
 
 <template>
-    <div class="question">
-        <h3>{{ question.question }}</h3>
-        <ul>
-            <li v-for="(choice, index) in randomChoices" :key="choice">
-                <Reponse :id="`reponse${index}`" :disabled="noReponse" :value="choice" v-model="reponse"/>
-            </li>
-        </ul>
-        {{ reponse }}
-        <button :disabled="!noReponse" @click="emits('reponse', reponse)">Question suivante</button>
-    </div>
+  <div class="question">
+    <h3>{{ question.question }}</h3>
+    <ul>
+      <li v-for="(choice, index) in randomChoices" :key="choice">
+        <Reponse
+          :id="`reponse${index}`"
+          :disabled="noReponse"
+          :value="choice"
+          @change="onReponse"
+          :correctReponse="question.correct"
+        />
+      </li>
+    </ul>
+    {{ reponse }}
+    <!-- <button :disabled="!noReponse" @click="emits('reponse', reponse)">
+      Question suivante
+    </button> -->
+  </div>
 </template>
 
 <style>
 .question {
-    padding-top: 2rem;
+  padding-top: 2rem;
 }
 
 .question button {
-    margin-left: auto;
-    display: block;
+  margin-left: auto;
+  display: block;
 }
 </style>
--- src/components/Reponse.vue
+++ src/components/Reponse.vue
@@ -1,22 +1,49 @@
 <script setup>
-    const props = defineProps({
-        id: String,
-        disabled: Boolean,
-        value: String,
-    })
-    const model = defineModel()
+import { computed } from "vue";
+
+const props = defineProps({
+  id: String,
+  disabled: Boolean,
+  value: String,
+  correctReponse: String,
+});
+const model = defineModel();
+const emits = defineEmits(['change'])
+const onChange = (event) => {
+  emits('change', event)
+}
+const classes = computed(() => ({
+  disabled: props.disabled,
+  right: props.disabled && props.value === props.correctReponse,
+  wrong: props.disabled && props.value !== props.correctReponse && model.value === props.value,
+}));
 </script>
 
 <template>
-  <label :for="id">
+  <label :for="id" :class="classes">
     <input
-    :disabled="disabled"
+      :disabled="disabled"
       :id="id"
       type="radio"
       name="reponse"
       v-model="model"
       :value="value"
+      @change="onChange"
     />
     {{ value }}
   </label>
 </template>
+
+<style>
+.right {
+  color: green;
+  opacity: 1;
+}
+.wrong {
+  color: red;
+  opacity: 1;
+}
+.disabled {
+  opacity: 0.5;
+}
+</style>
